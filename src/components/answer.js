ad0b282768335454e0b934d697704b62b794b806
AndrewRMorgan <andrew.r.morgan@gmail.com>
2017-11-06 21:14:26 +0000

    removed redundant files and code

---
+++ src/components/answer.js
@@ -0,0 +1,8 @@
+import React, { Component } from 'react';
+import ReactDOM from 'react-dom';
+
+const Answer = () => {
+
+}
+
+export default Answer;
--- src/index.js
+++ src/index.js
@@ -1,8 +1,29 @@
-import React from 'react';
+import React, { Component } from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import registerServiceWorker from './registerServiceWorker';
+import ProgressBar from './components/progress_bar';
+import Question from './components/question';
+import Input from './components/input';
+import Answer from './components/answer';
+
+class App extends Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      currentQuestionNumber: 0,
+      currentQuestion: null,
+      correctAnswer: null,
+      userAnswer: null,
+    };
+  }
+
+  render() {
+    return (
+      <div>
+
+      </div>
+    )
+  }
+}
 
 ReactDOM.render(<App />, document.getElementById('root'));
-registerServiceWorker();
